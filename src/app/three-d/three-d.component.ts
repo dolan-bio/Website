68ed753497effbf1885c9f5b207e17011c766071
Dolan Miu <dolan_miu@hotmail.com>
2017-07-09 17:08:33 +0100

    Added 3d Scene

--- src/app/three-d/three-d.component.ts
+++ src/app/three-d/three-d.component.ts
@@ -1,15 +1,22 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
 
+import { Stage } from '@dolan-bio/3d-scene';
+
 @Component({
     selector: 'app-three-d',
     templateUrl: './three-d.component.html',
     styleUrls: ['./three-d.component.scss']
 })
 export class ThreeDComponent implements OnInit {
+    private stage: Stage;
 
     constructor(private el: ElementRef) {
+        this.stage = new Stage();
     }
 
     public ngOnInit(): void {
+        this.stage.run(this.el.nativeElement);
+        this.stage.setSize(document.body.clientWidth, window.innerHeight);
+
     }
 }
